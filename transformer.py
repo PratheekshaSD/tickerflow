0bab61f846765be861fc9eeff16b5ccf5b147523
Pratheeksha <pratheekshadodamani@gmail.com>
2026-06-20 12:33:03 +0530

    Fixed column names in Transfomer to match database schema

--- transformer.py
+++ transformer.py
@@ -13,19 +13,20 @@ class Transformer:
         return df
     
     def compute_metrics(self,df):
-        df['Daily return']=(df['Close']-df['Close'].shift(1))/df['Close'].shift(1)
-        df['average7'] = df['Close'].rolling(window=7).mean() #moving average
-        df['average21']=df['Close'].rolling(window=21).mean()
+        df['daily_return']=(df['Close']-df['Close'].shift(1))/df['Close'].shift(1)
+        df['avg7'] = df['Close'].rolling(window=7).mean() #moving average
+        df['avg21']=df['Close'].rolling(window=21).mean()
         return df
+    
+# ****TESTCODE***
+# if __name__=="__main__":
+#     from extractor import Extractor
+#     e=Extractor()
+#     t=Transformer()
 
-if __name__=="__main__":
-    from extractor import Extractor
-    e=Extractor()
-    t=Transformer()
-
-    raw_df=e.fetch_ticker_data("TCS.NS")
-    cleaned_df =t.clean(raw_df,"TCS.NS")
-    final_df=t.compute_metrics(cleaned_df)
-    print(final_df.head())
-    print(final_df.columns)
-    print(final_df[['Close', 'average7', 'average21']].tail(10))
+#     raw_df=e.fetch_ticker_data("TCS.NS")
+#     cleaned_df =t.clean(raw_df,"TCS.NS")
+#     final_df=t.compute_metrics(cleaned_df)
+#     print(final_df.head())
+#     print(final_df.columns)
+#     print(final_df[['Close', 'average7', 'average21']].tail(10))
