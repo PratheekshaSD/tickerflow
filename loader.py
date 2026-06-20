206d30396106e046f5484f4f4a67730db02dba66
Pratheeksha <pratheekshadodamani@gmail.com>
2026-06-20 17:19:04 +0530

    Completed Loader class with upsert and run logging

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2.extras import execute_values
+from datetime import datetime
 import os
 
 class Loader:
@@ -61,3 +62,52 @@ class Loader:
 
         self.connection.commit()
         cursor.close()
+
+    def upsert_stock_data(self,df):
+        cursor=self.connection.cursor()
+        sql="""INSERT INTO stock_prices
+                    (date,ticker,open,high,low,close,volume,daily_return,avg7,avg21)
+                    VALUES %s
+                    ON CONFLICT(date,ticker)
+                    DO UPDATE SET
+                    open =EXCLUDED.open,
+                    high=EXCLUDED.high,
+                    low=EXCLUDED.low,
+                    close=EXCLUDED.close,
+                    volume=EXCLUDED.volume,
+                    daily_return=EXCLUDED.daily_return,
+                    avg7=EXCLUDED.avg7,
+                    avg21=EXCLUDED.avg21;
+                    """
+            
+        values=[
+                (
+                    row['Date'],
+                    row['Ticker'],
+                    row['Open'],
+                    row['High'],
+                    row['Low'],
+                    row['Close'],
+                    row['Volume'],
+                    row['daily_return'],
+                    row["avg7"],
+                    row['avg21']
+                )
+                for _, row in df.iterrows()
+            ]  
+          
+        execute_values(cursor,sql,values)
+        self.connection.commit()
+        cursor.close()
+
+        return len(values) 
+     
+    def log_run(self, ticker, status,rows_fetched=0,error_messages=None):
+          cursor=self.connection.cursor()
+          cursor.execute(""" 
+        INSERT INTO run_log(run_timestamp, ticker, status, rows_fetched, error_messages)
+        VALUES (%s, %s, %s, %s, %s)
+            """,(datetime.now(), ticker, status, rows_fetched, error_messages))
+          self.connection.commit()
+          cursor.close()
+
